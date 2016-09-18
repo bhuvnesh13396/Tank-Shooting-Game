37626804b4595742486ccd40df57b149319b6fc0
bhuvnesh13396 <bhuvnesh13396@gmail.com>
2016-09-18 09:12:22 +0530

    Implemented aliens,collision detection,game life lines

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from alien import Alien
+from game_stats import GameStats
+
 
 def run_game():
 	# Initialize game object and create a screen object
@@ -23,29 +26,32 @@ def run_game():
 	screen=pygame.display.set_mode((ai_settings.screen_width , ai_settings.screen_height))
 	pygame.display.set_caption("Tank Shooting")
 
-	# MAke a ship
+	# Create an instance to store game statistics.
+	stats = GameStats(ai_settings)
+
+	# Make a ship,a group of bullets , and group of aliens
 	ship=Ship(ai_settings,screen)
-
-	# Make group to store bullets in.
 	bullets=Group()
-	
+	aliens=Group()
 
-	
+	# Create fleet of aliens.
+	gf.create_fleet(ai_settings,screen,ship,aliens)	
 
 	# Start the main loop for the game.
 	while True:
 
 		
 		gf.check_events(ai_settings,screen,ship,bullets)
-		ship.update()
 
-		gf.update_bullets(bullets)
+		if stats.game_active:
+			ship.update()
 
-		gf.update_screen(ai_settings,screen,ship,bullets)
+			gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
+			gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
+
+		gf.update_screen(ai_settings,screen,ship,aliens,bullets)
 		
-		# Make the most recently drawn screen visible.
-		pygame.display.flip()
-
+		
 
 
 run_game()	
